bb14dfeef0123f616fb6d178c766be5537791ae5
Marek Rozmus <marek.rozmus@outlook.com>
2023-04-23 11:43:59 +0100

    Fix for hiding actions if other list item used

--- src/SwipeableList.js
+++ src/SwipeableList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 
@@ -10,33 +10,59 @@ export const Type = {
   MS: Symbol('MS'),
 };
 
-const SwipeableList = ({
-  actionDelay = 0,
-  children,
-  className = '',
-  fullSwipe = false,
-  destructiveCallbackDelay = 1000,
-  style,
-  type = Type.ANDROID,
-  Tag = 'div',
-  scrollStartThreshold,
-  swipeStartThreshold,
-  threshold = 0.5,
-}) => (
-  <Tag className={clsx('swipeable-list', className)} style={style}>
-    {React.Children.map(children, child =>
-      React.cloneElement(child, {
-        actionDelay,
-        destructiveCallbackDelay,
-        fullSwipe,
-        listType: type,
-        scrollStartThreshold,
-        swipeStartThreshold,
-        threshold,
-      })
-    )}
-  </Tag>
-);
+class SwipeableList extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.itemsMap = {};
+    this.clickedItem = this.clickedItem.bind(this);
+  }
+
+  clickedItem(itemId) {
+    Object.keys(this.itemsMap).forEach(listItem => {
+      if (listItem !== itemId) {
+        this.itemsMap[listItem]();
+      }
+    });
+  }
+
+  render() {
+    const {
+      actionDelay = 0,
+      children,
+      className = '',
+      fullSwipe = false,
+      destructiveCallbackDelay = 1000,
+      style,
+      type = Type.ANDROID,
+      Tag = 'div',
+      scrollStartThreshold,
+      swipeStartThreshold,
+      threshold = 0.5,
+    } = this.props;
+
+    return (
+      <Tag className={clsx('swipeable-list', className)} style={style}>
+        {React.Children.map(children, (child, index) =>
+          React.cloneElement(child, {
+            actionDelay,
+            destructiveCallbackDelay,
+            fullSwipe,
+            listType: type,
+            scrollStartThreshold,
+            swipeStartThreshold,
+            threshold,
+            clickedCallback: this.clickedItem,
+            id: `listItem-${index}`,
+            resetState: func => {
+              this.itemsMap[`listItem-${index}`] = func;
+            },
+          })
+        )}
+      </Tag>
+    );
+  }
+}
 
 SwipeableList.propTypes = {
   actionDelay: PropTypes.number,
