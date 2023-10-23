39328e61b2ff2c584f2240b52efaddd59d00848d
Daniel Mashukov <63494820+kustomq@users.noreply.github.com>
2023-10-23 18:46:57 +0300

    Update module.d.ts

--- src/module.d.ts
+++ src/module.d.ts
@@ -189,7 +189,7 @@ interface SwipeableListItemProps {
   /**
    * Fired every time swipe progress changes. The reported `progress` value is always an integer in range 0 to 100 inclusive.
    */
-  onSwipeProgress?: (progress: number, dragDirection: string) => void;
+  onSwipeProgress?: (progress:number, dragDirection: string) => void;
   /**
    * Fired after swipe has started (after drag gesture passes the `swipeStartThreshold` distance in pixels).
    */
